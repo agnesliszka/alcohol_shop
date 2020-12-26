40fbb197b46b05844f8d227fca8508f2b7f368b0
Agnieszka <agnesliszk@gmail.com>
2020-12-26 17:40:34 +0100

    Import vuex

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import "@babel/polyfill";
 import "mutationobserver-shim";
 import Vue from "vue";
+import Vuex from "vuex";
 import "./plugins/bootstrap-vue";
 import App from "./App.vue";
 import "bootstrap";
@@ -9,10 +10,19 @@ import ElementUI from "element-ui";
 import "element-ui/lib/theme-chalk/index.css";
 import locale from "element-ui/lib/locale/lang/en";
 
+Vue.use(Vuex);
 Vue.use(ElementUI, { locale });
+
+const store = new Vuex.Store({
+  state: {},
+  getters: {},
+  mutations: {},
+  actions: {},
+});
 
 Vue.config.productionTip = false;
 
 new Vue({
+  store,
   render: (h) => h(App),
 }).$mount("#app");
