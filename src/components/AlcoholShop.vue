5e389c55a6cba039239192633577ee7fc8c7d2eb
Agnieszka <agnesliszk@gmail.com>
2020-12-27 17:28:46 +0100

    Working version

--- src/components/AlcoholShop.vue
+++ src/components/AlcoholShop.vue
@@ -2,7 +2,40 @@
   <div class="main">
     <h1>Alcohol shop</h1>
 
-    <ToBeBought :shopItems="shopItems" />
+    <div>
+      <div>
+        <el-button type="info" @click="enableBuyingItems">Buy items</el-button>
+        <el-button type="info" @click="enableSeeingItemsBought"
+          >Shopping cart</el-button
+        >
+      </div>
+      <div class="toBeBought" v-if="itemsToBeBoughtPage">
+        <el-checkbox-group v-model="itemsBought">
+          <el-checkbox
+            v-for="(item, index) in shopItems"
+            :label="item"
+            :key="index"
+            ><el-row>
+              <el-card :body-style="{ padding: '0px' }">
+                <img :src="item.image" class="image" />
+                <div style="padding: 14px">
+                  <span>{{ item.name }}</span>
+                  <div class="bottom clearfix">
+                    <el-button
+                      type="success"
+                      icon="el-icon-message"
+                      circle
+                      @click="buyItems"
+                      >Buy</el-button
+                    >
+                  </div>
+                </div>
+              </el-card>
+            </el-row>
+          </el-checkbox>
+        </el-checkbox-group>
+      </div>
+    </div>
     <div class="shopping cart" v-if="shoppingCartPage">
       <div v-for="(item, index) in itemsBought" :label="item" :key="index">
         {{ item.name }}
@@ -12,16 +45,19 @@
 </template>
 
 <script>
-import ToBeBought from "./ToBeBought.vue";
+// import ToBeBought from "./ToBeBought.vue";
 
 export default {
   name: "AlcoholShop",
 
   components: {
-    ToBeBought,
+    // ToBeBought,
   },
   data() {
     return {
+      itemsBought: [],
+      itemsToBeBoughtPage: true,
+      shoppingCartPage: false,
       shopItems: [
         {
           name: "wine",
@@ -56,7 +92,24 @@ export default {
       ],
     };
   },
-  methods: {},
+  methods: {
+    buyItems() {
+      console.log(this.itemsBought);
+      this.tooglePages();
+    },
+    tooglePages() {
+      this.itemsToBeBoughtPage = !this.itemsToBeBoughtPage;
+      this.shoppingCartPage = !this.shoppingCartPage;
+    },
+    enableBuyingItems() {
+      this.itemsToBeBoughtPage = true;
+      this.shoppingCartPage = false;
+    },
+    enableSeeingItemsBought() {
+      this.itemsToBeBoughtPage = false;
+      this.shoppingCartPage = true;
+    },
+  },
 };
 </script>
 
@@ -73,5 +126,41 @@ h1 {
   height: 90vh;
   border-radius: 5%;
   opacity: 94%;
+}
+.toBeBought {
+  margin-top: 20px;
+  font-size: 20px;
+  text-decoration: none;
+}
+.time {
+  font-size: 13px;
+  color: #999;
+}
+
+.bottom {
+  margin-top: 13px;
+  line-height: 12px;
+}
+
+.button {
+  padding: 0;
+  float: right;
+}
+
+.image {
+  margin-top: 5%;
+  margin-left: 15%;
+  width: 70px;
+  height: 70px;
+  display: block;
+}
+.clearfix:before,
+.clearfix:after {
+  display: table;
+  content: "";
+}
+
+.clearfix:after {
+  clear: both;
 }
 </style>
